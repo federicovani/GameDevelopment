ccdccb707ce5ccdf0914be34c16724e826cdd671
Federico <federico.vani@icloud.com>
2024-12-19 14:38:13 +0100

    inizio killzone

--- mobile/scripts/globali/knight.gd
+++ mobile/scripts/globali/knight.gd
@@ -6,6 +6,9 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+func die():
+	animated_sprite.play("death")
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
