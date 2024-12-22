140209828eecb5b7edc944e1fc558c4a17b4926c
Federico <federico.vani@icloud.com>
2024-12-22 13:08:02 +0100

    Attacchi e scheletro (work in progress)

--- mobile/scripts/globali/Knight/sword.gd
+++ mobile/scripts/globali/Knight/sword.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 @export var damage : int = 20
 
+func _ready():
+	monitoring = false
+
 func _on_body_entered(body: Node2D) -> void:
 	for child in body.get_children():
 		if child is damageable:
 			child.hit(damage)
-			
-	print("ci sto")
+			#print_debug(body.name + " took " + str(damage) + " damage")
+	
--- mobile/scripts/globali/skeleton.gd
+++ mobile/scripts/globali/skeleton.gd
@@ -1,9 +1,14 @@
 class_name damageable extends Node
 
-@export var health : float = 50
+@export var health : float = 50 :
+	get:
+		return health
+	set(value):
+		SignalBus.emit_signal("on_health_changed", get_parent(), value - health)
+		health = value
 
 func hit(damage : int):
 	health -= damage
-	
+	#print_debug("Health: " + str(health))
 	if(health <= 0):
 		get_parent().queue_free()
