996ce5b52574b75ec8fa7cbb03fe1410d4680290
Federico <federico.vani@icloud.com>
2025-01-08 17:19:08 +0100

    circa fixato l'offset delle animazioni degli attacchi, ma ancora rimane un millisecondo di scatto alla fine

--- mobile/scripts/globali/State Machine/attack_state.gd
+++ mobile/scripts/globali/State Machine/attack_state.gd
@@ -7,7 +7,6 @@ class_name attack extends State
 @export var sprite : SpriteAttackOffset
 
 func on_enter():
-	#set_attack_offset()
 	playback.travel(character.attack_animation)
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
@@ -16,7 +15,6 @@ func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 		if(!attack_zone.has_overlapping_bodies()):
 			next_state = character.chase_state
 		else:
-			#reset_offset()
 			buffer.start()
 
 func _on_buffer_timer_timeout() -> void:
@@ -34,6 +32,3 @@ func set_attack_offset():
 
 func reset_offset():
 	sprite.offset = sprite.default_offset
-
-#func on_exit():
-	#reset_offset()
