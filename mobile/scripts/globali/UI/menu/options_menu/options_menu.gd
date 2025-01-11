e961584b58db22aa9eb2212f0e9589ebe8d2e23d
Federico <federico.vani@icloud.com>
2025-01-11 21:10:32 +0100

    options menu navigabile con la tastiera (destra, sinistra, esc)

--- mobile/scripts/globali/UI/menu/options_menu/options_menu.gd
+++ mobile/scripts/globali/UI/menu/options_menu/options_menu.gd
@@ -1,12 +1,16 @@
 class_name OptionsMenu extends Control
 
+@onready var settings_tab_container: SettingTabContainer = $MarginContainer/VBoxContainer/SettingsTabContainer
+
 signal exit_options_menu
 
 func _ready() -> void:
 	#Cant do anything before the process is set to true, prevent bugs and ghost buttons
+	settings_tab_container.exit_options_menu.connect(on_exit_button_down)
 	set_process(false)
 
-func _on_exit_button_down() -> void:
+func on_exit_button_down() -> void:
 	exit_options_menu.emit()
+	#Save all the settings
 	SettingsSignalBus.emit_set_settings_dictionary(SettingsDataContainer.create_storage_dictionary())
 	set_process(false)
