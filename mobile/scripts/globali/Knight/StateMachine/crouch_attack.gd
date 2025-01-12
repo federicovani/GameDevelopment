b55ac3cd8a06a10e02e95099997fb776421b20f8
Federico <federico.vani@icloud.com>
2025-01-12 19:18:29 +0100

    Risolto bug che ti faceva alzare anche se avevi un muro in testa, facendoti incastrare

--- mobile/scripts/globali/Knight/StateMachine/crouch_attack.gd
+++ mobile/scripts/globali/Knight/StateMachine/crouch_attack.gd
@@ -5,7 +5,7 @@ func on_enter():
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if(anim_name == character.crouch_attack_animation):
-		if character.is_crouching:
+		if character.is_crouching || !character.crouch_state.can_standup():
 			next_state = character.crouch_state
 		else:
 			next_state = character.idle_state
--- mobile/scripts/globali/Knight/StateMachine/crouching.gd
+++ mobile/scripts/globali/Knight/StateMachine/crouching.gd
@@ -1,15 +1,20 @@
 class_name crouching extends State
 
 @export var sprite: Sprite2D
+@export var crouch_area : Area2D
 @export var knight_facing_collision_shape : FacingCollisionShapeKnight
 @export var sword_facing_collision_shape : FacingCollisionShapeKnight
-	
+@export var crouch_area_facing_collision_shape : FacingCollisionShape
+
 func on_enter():
 	playback.travel(character.crouch_animation)
+	crouch_area.monitoring = true
 
 func state_process(delta):
 	if(!character.is_on_floor()):
 		next_state = character.falling_state
+	if(!character.is_crouching && can_standup()):
+		walk()
 	if get_parent().check_if_can_move():
 			character.direction = Input.get_vector("move_left", "move_right", "jump", "ui_down")
 			
@@ -28,7 +33,7 @@ func _input(event: InputEvent) -> void:
 		character.is_crouching = false
 
 func state_input(event: InputEvent):
-	if(event.is_action_released("crouch")):
+	if(event.is_action_released("crouch") && can_standup()):
 		walk()
 	if(event.is_action_pressed("attack")):
 		attack()
@@ -38,6 +43,9 @@ func walk():
 
 func attack():
 	next_state = character.crouch_attack_state
+
+func can_standup() -> bool:
+	return !crouch_area.has_overlapping_bodies()
 
 #Change sprite orientation
 func update_facing_direction():
@@ -55,6 +63,11 @@ func on_player_facing_direction_changed(facing_right : bool):
 	if(facing_right):
 		knight_facing_collision_shape.position = knight_facing_collision_shape.crouch_facing_right_position
 		sword_facing_collision_shape.position = sword_facing_collision_shape.crouch_facing_right_position
+		crouch_area_facing_collision_shape.position = crouch_area_facing_collision_shape.facing_right_position
 	else:
 		knight_facing_collision_shape.position = knight_facing_collision_shape.crouch_facing_left_position
 		sword_facing_collision_shape.position = sword_facing_collision_shape.crouch_facing_left_position
+		crouch_area_facing_collision_shape.position = crouch_area_facing_collision_shape.facing_left_position
+
+func on_exit():
+	crouch_area.monitoring = false
