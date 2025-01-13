e13312f45f3c9af12bdcbf4607de6294196d6134
Federico <federico.vani@icloud.com>
2025-01-13 21:08:54 +0100

    fix

--- mobile/scripts/globali/autoloads/signal_bus.gd
+++ mobile/scripts/globali/autoloads/signal_bus.gd
@@ -5,12 +5,11 @@ signal on_health_changed(node : Node, amount_changed : int)
 
 @warning_ignore("unused_signal")
 signal portal_crossed()
-	
+
 signal on_coin_collected(value : int)
-
 signal update_coin_label(value : int)
-
-
+	
+	
 func emit_on_coin_collected(value : int):
 	on_coin_collected.emit(value)
 
