87b2459e6280363a3f8c3dad42e21b1763d1e13b
Federico <federico.vani@icloud.com>
2025-01-20 15:20:39 +0100

    jump buffer

--- mobile/scripts/globali/Knight/StateMachine/falling.gd
+++ mobile/scripts/globali/Knight/StateMachine/falling.gd
@@ -8,18 +8,26 @@ class_name falling extends State
 @export var raycast_wall_check : RayCast2D
 @export var marker : Marker2D
 
+var jump_buffer = false
+var jump_buffer_timer : float = 0.1
+
 func on_enter():
 	playback.travel(character.jump_between_animation)
+	jump_buffer = false
 	ledge_grab.disabled = false
 
 func state_process(_delta):
 	check_ledge_grab()
 	if(floor_check.is_colliding()):
-		if character.is_crouching:
-			playback.travel(character.crouch_transition_animation)
-			next_state = character.crouch_state
+		if jump_buffer:
+			next_state = character.jumping_state
+			jump_buffer = false
 		else:
-			next_state = character.idle_state
+			if character.is_crouching:
+				playback.travel(character.crouch_transition_animation)
+				next_state = character.crouch_state
+			else:
+				next_state = character.idle_state
 		#Add the dust effect when landing
 		var instance = dust.instantiate()
 		instance.position = marker.position
@@ -29,6 +37,11 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if !character.is_on_floor() && get_parent().current_state != character.wall_hang_state:
 		character.velocity.y += get_gravity() * delta
+
+func state_input(event: InputEvent):
+	if(event.is_action_pressed("jump")):
+		jump_buffer = true
+		get_tree().create_timer(jump_buffer_timer).timeout.connect(_on_buffer_jump_timeout)
 
 func check_ledge_grab():
 	if raycast_wall_check.is_colliding():
@@ -42,6 +55,9 @@ func get_gravity():
 	if(character.velocity.y) < 0:
 		return character.gravity
 	return character.falling_gravity
+
+func _on_buffer_jump_timeout():
+	jump_buffer = false
 
 func on_exit():
 	ledge_grab.disabled = true
