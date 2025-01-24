1aa7acbc4d51f14e3e0fe73b68a26f792eacb288
Federico <federico.vani@icloud.com>
2025-01-24 15:54:35 +0100

    fix

--- mobile/scripts/livello_federico/livello_federico.gd
+++ mobile/scripts/livello_federico/livello_federico.gd
@@ -1,10 +1,4 @@
 extends Node2D
 
-@export var next_level : String = "res://scenes/livello_sara/livello_sara.tscn"
-
 func _ready() -> void:
 	Global.current_level = "livello_federico"
-	#SignalBus.connect("portal_crossed", on_portal_crossed)
-
-#func on_portal_crossed():
-	#get_tree().change_scene_to_file(next_level)
