e6a9b40e2f55277264c74349b84c64b0e46c18e6
Federico <federico.vani@icloud.com>
2025-01-27 20:41:09 +0100

    animazioni tasti main menu

--- mobile/scripts/globali/UI/menu/main_menu.gd
+++ mobile/scripts/globali/UI/menu/main_menu.gd
@@ -4,8 +4,31 @@ class_name MainMenu extends Control
 @onready var options_menu: OptionsMenu = $OptionsMenu
 @onready var margin_container: MarginContainer = $MarginContainer
 
+@onready var start: Button = $MarginContainer/VBoxContainer/ButtonsContainer/VBoxContainer/Start
+@onready var options: Button = $MarginContainer/VBoxContainer/ButtonsContainer/VBoxContainer/Options
+@onready var quit: Button = $MarginContainer/VBoxContainer/ButtonsContainer/VBoxContainer/Quit
+
 func _ready() -> void:
 	options_menu.exit_options_menu.connect(on_exit_options_menu)
+
+func _process(_delta: float) -> void:
+	update_button_scale()
+
+func update_button_scale():
+	button_hover(start, 1.15, 0.2)
+	button_hover(options, 1.15, 0.2)
+	button_hover(quit, 1.15, 0.2)
+
+func button_hover(button : Button, tween_amt, duration):
+	button.pivot_offset = button.size / 2
+	if button.is_hovered():
+		tween(button, "scale", Vector2.ONE * tween_amt, duration)
+	else:
+		tween(button, "scale", Vector2.ONE, duration)
+
+func tween(button, property, amount, duration):
+	var tween = create_tween()
+	tween.tween_property(button, property, amount, duration)
 
 func _on_start_button_down() -> void:
 	get_tree().change_scene_to_packed(start_level)
