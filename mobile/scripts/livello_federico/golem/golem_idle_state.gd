d5408f64dd9f2dd4e001cb7283b6b8b483c292f1
Federico <federico.vani@icloud.com>
2025-01-30 18:31:01 +0100

    inizio golem livello federico

---
+++ mobile/scripts/livello_federico/golem/golem_idle_state.gd
@@ -0,0 +1,17 @@
+extends State
+
+@export var awakening_zone : Area2D
+@export var attack_zone_collision_shape : CollisionShape2D
+
+func _ready() -> void:
+	attack_zone_collision_shape.disabled = true
+
+func on_enter():
+	playback.travel(character.idle_animation)
+	
+func _on_awakening_zone_body_entered(body: Node2D) -> void:
+	if(get_parent().current_state == self):
+		await character.berserk_tween(1.0)
+		attack_zone_collision_shape.disabled = false
+		playback.travel(character.walk_animation)
+		next_state = character.chase_state
