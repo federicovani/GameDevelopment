31be71fc8c132c5e08b3f5286f43352fd574cb05
Federico <federico.vani@icloud.com>
2025-01-31 19:18:41 +0100

    fix player che rimaneva in jumping state saltando su una piattaforma che va verso l'alto

--- mobile/scripts/globali/Knight/StateMachine/jumping.gd
+++ mobile/scripts/globali/Knight/StateMachine/jumping.gd
@@ -1,8 +1,11 @@
 class_name jumping extends State
 
+@onready var buffer_timer: Timer = $BufferTimer
+
 var released : bool = false
 
 func on_enter():
+	buffer_timer.start()
 	character.velocity.y = character.jump_velocity
 	released = false
 	playback.travel(character.jump_start_animation)
@@ -11,6 +14,8 @@ func on_enter():
 func state_process(_delta):
 	if(character.velocity.y > 0):
 		next_state = character.falling_state
+	if(buffer_timer.is_stopped() && character.is_on_floor()):
+		next_state = character.idle_state
 
 func state_input(event: InputEvent):
 	#Different jump heights based on the time the space bar is pressed
