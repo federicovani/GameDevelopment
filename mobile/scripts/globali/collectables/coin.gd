76e85733712aeb6366e414b847b92c02e380694a
Federico <federico.vani@icloud.com>
2025-02-01 22:45:22 +0100

    livello federico + suoni monete e diamanti

--- mobile/scripts/globali/collectables/coin.gd
+++ mobile/scripts/globali/collectables/coin.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+var picked : bool = false
+
+func _ready() -> void:
+	picked = false
+
 func _on_body_entered(_body: Node2D) -> void:
-	SignalBus.emit_on_coin_collected(1)
-	queue_free()
+	if !picked:
+		picked = true
+		visible = false
+		SignalBus.emit_on_coin_collected(1)
+		audio_stream_player_2d.play()
+		await audio_stream_player_2d.finished
+		queue_free()
--- mobile/scripts/globali/collectables/diamond.gd
+++ mobile/scripts/globali/collectables/diamond.gd
@@ -1,6 +1,18 @@
 extends Area2D
 
+var picked : bool = false
+
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+func _ready() -> void:
+	picked = false
+
 func _on_body_entered(_body: Node2D) -> void:
-	SignalBus.emit_on_coin_collected(10)
-	SignalBus.emit_diamond_collected()
-	queue_free()
+	if !picked:
+		picked = true
+		visible = false
+		SignalBus.emit_on_coin_collected(10)
+		SignalBus.emit_diamond_collected()
+		audio_stream_player_2d.play()
+		await audio_stream_player_2d.finished
+		queue_free()
