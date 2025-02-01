84cc4930ea9a37905e77f46f3294fc496d01db74
Federico <federico.vani@icloud.com>
2025-02-01 21:14:09 +0100

    livello federico

--- mobile/scripts/livello_federico/livello_federico.gd
+++ mobile/scripts/livello_federico/livello_federico.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
+@onready var rising_lava_animation_player: AnimationPlayer = $Layers/rising_lava/RisingLavaAnimationPlayer
+
 func _ready() -> void:
 	Global.current_level = "livello_federico"
+
+
+func _on_lava_rising_trigger_body_entered(body: Node2D) -> void:
+	if(body == Global.playerBody):
+		SignalBus.emit_camera_shook(3)
+		rising_lava_animation_player.play("rise")
--- mobile/scripts/livello_federico/wooden_falling_platform.gd
+++ mobile/scripts/livello_federico/wooden_falling_platform.gd
@@ -4,12 +4,15 @@ extends StaticBody2D
 @onready var respawn_timer: Timer = $RespawnTimer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var trigger_area: Area2D = $TriggerArea
 
 func _on_trigger_area_body_entered(body: Node2D) -> void:
-	if body.name == "knight":
+	if body == Global.playerBody:
 		timer.start()
+		animation_player.active = true
 		animation_player.play("shake")
-
+		trigger_area.set_deferred("monitoring", false)
+	
 
 func _on_timer_timeout() -> void:
 	animation_player.play("fall")
@@ -24,3 +27,4 @@ func _on_respawn_timer_timeout() -> void:
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1, 0.4)
 	collision_shape_2d.disabled = false
+	trigger_area.set_deferred("monitoring", true)
