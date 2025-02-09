9d6761673b012ade045bfd2331ff8b1150cf64bc
Sara <sar.valletta@stud.uniroma3.it>
2025-02-09 19:19:04 +0100

    fix

--- mobile/scripts/livello_sara/pushable_object.gd
+++ mobile/scripts/livello_sara/pushable_object.gd
@@ -3,17 +3,25 @@ extends RigidBody2D
 @export var push_force: float = 140.0
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+var is_playing: bool = false
 var is_being_pushed: bool = false
 
 func _physics_process(delta: float) -> void:
 	# Controlla se l'oggetto è in movimento
 	if linear_velocity.x != 0 and not is_being_pushed:
 		is_being_pushed = true
-		audio_stream_player_2d.play()
 	elif linear_velocity.x == 0 and is_being_pushed:
 		is_being_pushed = false
-		audio_stream_player_2d.stop()
+	audio_manager()
 
+func audio_manager():
+	if is_being_pushed:
+		if !is_playing:
+			audio_stream_player_2d.play()
+			is_playing = true
+	else:
+		is_playing = false
+		# fade_out_audio()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == Global.playerBody:
