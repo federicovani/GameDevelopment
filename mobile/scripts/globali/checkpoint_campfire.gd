7018f58e191b863b9c094ea875ebcba0136993b8
Federico <federico.vani@icloud.com>
2025-02-13 18:06:32 +0100

    suono campfire

--- mobile/scripts/globali/checkpoint_campfire.gd
+++ mobile/scripts/globali/checkpoint_campfire.gd
@@ -2,17 +2,21 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var light: PointLight2D = $PointLight2D
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	if LevelStats.is_checkpoint_taken():
 		animation_player.play("fire")
 		light.enabled = true
+		audio_stream_player.play()
 	else:
 		animation_player.play("no_fire")
 		light.enabled = false
+		audio_stream_player.stop()
 
 func _on_body_entered(body: Node2D) -> void:
 	if(body == Global.playerBody && !LevelStats.is_checkpoint_taken()):
 		SignalBus.emit_checkpoint_taken()
 		animation_player.play("fire")
 		light.enabled = true
+		audio_stream_player.play()
